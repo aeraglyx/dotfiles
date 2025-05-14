9cd05e4c40dc0f22be24fbb412ff3c1a2a70d2ed
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2025-05-14 05:29:53 +0200

    Update hypr and waybar cfg

--- waybar/.config/waybar/scripts/song_info.py
+++ waybar/.config/waybar/scripts/song_info.py
@@ -1,5 +1,7 @@
 import subprocess
 
+MAX_LENGTH = 48
+
 def is_song_playing():
     args = ["playerctl", "--player=spotify", "status"]
     status = subprocess.check_output(args, stderr=subprocess.DEVNULL).decode().strip()
@@ -7,7 +9,10 @@ def is_song_playing():
 
 def get_song_info():
     args = ["playerctl", "--player=spotify", "metadata", "--format", "{{ artist }} | {{ title }}"]
-    return subprocess.check_output(args).decode().strip()
+    song_info = subprocess.check_output(args).decode().strip()
+    if len(song_info) > MAX_LENGTH:
+        song_info = song_info[:MAX_LENGTH-3].rstrip(".") + "..."
+    return song_info
 
 try:
     if is_song_playing():
