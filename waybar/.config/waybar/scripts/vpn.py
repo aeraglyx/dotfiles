f019a26a40ad786ee4896b39c8dca07fba3caae1
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2025-06-16 23:10:13 +0200

    Update waybar config

--- waybar/.config/waybar/scripts/vpn.py
+++ waybar/.config/waybar/scripts/vpn.py
@@ -6,17 +6,18 @@ def get_mullvad_status():
     vpn_info = subprocess.check_output(args).decode().strip()
     return json.loads(vpn_info)
 
-def get_vpn_state():
+def main():
     mullvad_status = get_mullvad_status()
     vpn_state = mullvad_status["state"]
-    return vpn_state
 
-try:
     out = {}
-    vpn_state = get_vpn_state()
     out["class"] = vpn_state
-    out["tooltip"] = vpn_state.capitalize()
-    print(json.dumps(out))
-except:
-    print("")
+    tooltip_text = vpn_state.capitalize()
+    if vpn_state == "connected":
+        vpn_country = mullvad_status["details"]["location"]["country"]
+        tooltip_text += f" | {vpn_country}"
+    out["tooltip"] = tooltip_text
 
+    print(json.dumps(out))
+
+main()
