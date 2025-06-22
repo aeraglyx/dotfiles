bb0779eedb5dbb1841121bd192c225d5ca413f16
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2025-06-23 01:43:12 +0200

    Update nvim options

--- nvim/.config/nvim/lua/config/core/options.lua
+++ nvim/.config/nvim/lua/config/core/options.lua
@@ -1,23 +1,26 @@
 vim.opt.swapfile = false
+vim.opt.undofile = true
 
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.breakindent = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.cindent = true
 
+vim.opt.wrap = false
+vim.opt.breakindent = true
+
 vim.opt.number = true
 vim.opt.relativenumber = false
+vim.opt.cursorline = true
 vim.opt.scrolloff = 4
+
 vim.opt.inccommand = "split"
-vim.opt.wrap = true
 vim.opt.signcolumn = "yes:1"
 vim.opt.statuscolumn = "%s%3l  "
-
-vim.opt.cursorline = true
+vim.opt.fillchars:append { eob = " " }
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
@@ -25,10 +28,10 @@ vim.opt.splitbelow = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
-vim.opt.fillchars:append { eob = " " }
-
-vim.g.have_nerd_font = true
 vim.opt.termguicolors = true
+vim.g.have_nerd_font = true
+vim.opt.laststatus = 3
+-- vim.opt.winborder = "rounded"
 
 vim.schedule(function()
     vim.opt.clipboard = "unnamedplus"
@@ -37,6 +40,7 @@ end)
 vim.diagnostic.config({
     virtual_text = true,
     signs = false,
+    float = { border = "rounded" },
 })
 
 vim.api.nvim_create_autocmd("BufEnter", {
