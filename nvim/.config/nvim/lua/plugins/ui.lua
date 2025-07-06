80f16212b8cabddbdd675e5f10cbf52dcd06590c
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2025-07-06 04:09:26 +0200

    nvim: re-enable indent-blankline.nvim

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -68,14 +68,13 @@ return {
             presets = { lsp_doc_border = true },
         },
     },
-    -- {
-    --     "lukas-reineke/indent-blankline.nvim",
-    --     event = {"BufReadPre", "BufNewFile"},
-    --     main = "ibl",
-    --     opts = {
-    --         indent = {
-    --             char = "|"
-    --         },
-    --     }
-    -- },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        event = {"BufReadPre", "BufNewFile"},
+        main = "ibl",
+        opts = {
+            indent = { char = "|" },
+            scope = { enabled = false },
+        }
+    },
 }
