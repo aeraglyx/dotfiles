d6576b92c6aeb4beb9b12b737590b9e2ccfdafb1
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2025-09-15 01:49:45 +0200

    scripts: small update

--- scripts/scripts/toggle_screen_recording.sh
+++ scripts/scripts/toggle_screen_recording.sh
@@ -1,5 +1,5 @@
 if pgrep "gpu-screen-reco" > /dev/null; then
     pkill -SIGINT "gpu-screen-reco"
 else
-    gpu-screen-recorder -w portal -restore-portal-session yes -f 30 -o ~/Videos/rec_$(date +"%Y%m%dT%H%M%S").mp4
+    gpu-screen-recorder -w portal -restore-portal-session yes -f 30 -o ~/videos/rec_$(date +"%Y%m%dT%H%M%S").mp4
 fi
