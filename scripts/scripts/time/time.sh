2de94e7c5d5f9104267a208963fb64cd822e12f3
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2026-04-01 13:33:05 +0200

    scripts: update time tracking

--- scripts/scripts/time/time.sh
+++ scripts/scripts/time/time.sh
@@ -1,22 +1,29 @@
-type="work"
-
 time_dir=$XDG_DATA_HOME/time
 status_file=$time_dir/status
-data_file=$time_dir/data/$type
 
-if [ ! -d $time_dir ]; then
-    mkdir -p $time_dir/data
-    touch $status_file
-fi
-
-if [ ! -f $data_file ]; then
-    touch $data_file
+if [ ! -d "$time_dir" ]; then
+    mkdir -p "$time_dir/data"
+    touch "$status_file"
 fi
 
 if [[ ! -s $status_file ]]; then
-    echo -n "$(date --iso-8601=s)" >> $data_file
-    echo "$type" > $status_file
+    type=$(
+        find "$time_dir/data" -mindepth 1 -printf "%T@_%P\n" |
+        sort -nr |
+        cut -d'_' -f2- |
+        rofi -sep "\n" -dmenu -p "project"
+    ) || exit 0
+
+    data_file=$time_dir/data/$type
+
+    if [ ! -f "$data_file" ]; then
+        touch "$data_file"
+    fi
+
+    echo -n "$(date --iso-8601=s)" >> "$data_file"
+    echo "$type" > "$status_file"
 else
-    echo "/$(date --iso-8601=s)" >> $data_file
-    echo -n > $status_file
+    data_file=$time_dir/data/$(cat "$status_file")
+    echo "/$(date --iso-8601=s)" >> "$data_file"
+    echo -n > "$status_file"
 fi
