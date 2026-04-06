509dbe09800c2c4c8f877abbffeaab40a8a4f387
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2026-04-06 13:29:28 +0200

    scripts: update main menu

--- scripts/scripts/stuff.sh
+++ scripts/scripts/stuff.sh
@@ -2,11 +2,13 @@ choices="weather
 time
 calculate
 countdown
-translate czech > en
-translate english > cz
+color picker
+calendar
+cz to en
+en to cz
 wallpaper"
 
-action=$(echo -n "$choices" | rofi -sep "\n" -dmenu -p stuff)
+action=$(echo -n "$choices" | rofi -sep "\n" -dmenu -p stuff -sort)
 
 case $action in
     "")
@@ -22,6 +24,12 @@ case $action in
         time_minutes="$(rofi -dmenu -p "minutes" -l 0)"
         sleep $((time_minutes * 60))
         notify-send "it's time, bro" --urgency=critical ;;
+
+    "color picker")
+        sleep 0.05 && hyprpicker ;;
+
+    "calendar")
+        ghostty --class=popup.app.high -e bash -c "python ~/projects/calendar/list.py; exec bash" ;;
 
     "translate czech > en")
         in="$(rofi -dmenu -p "cz" -l 0)"
